03590b2df3415ac56fe4962c2ec21bc62a26ce4e
Mohammed Ansir <1854992+MoAnsir@users.noreply.github.com>
2022-11-26 12:40:51 +0000

    random names

--- components/Stage.js
+++ components/Stage.js
@@ -3,40 +3,54 @@ import React, { useEffect, useState } from "react";
 
 const Stage = () => {
   const [character, setCharacter] = useState([]);
+  const [allCharactersNames, setAllCharactersNames] = useState([]);
   const [answer, setAnswer] = useState("");
   const [wrongAnswers, setWrongAnswers] = useState([]);
-  console.log("🚀 ~ file: Stage.js ~ line 5 ~ Stage ~ character", character);
+  console.log("🚀 ~ file: Stage.js ~ line 7 ~ Stage ~ allCharacters", allCharactersNames);
+  //console.log("🚀 ~ file: Stage.js ~ line 5 ~ Stage ~ character", character);
 
   //TODO -
-  // some times the character ID fails and doesn't return anything, Put a check in to see if it fails if so call it again until it gives back a success.
-  // Once it had show a character put them in a black list so they cant be shown again
+  // some times the character ID fails and doesn't return anything, Put a check in to see if it fails call the API again until it gives back a success.
+  // Once a character has been shown put them in a black list so they cant be shown again
   // Have a counter for the round
 
-  // const randomCharacters = (data) => {
-  //   for (let i = 0; i < 10; i++) {
-  //     setCharacters((state) => [...state, data[randomNum()]]);
-  //   }
-  // };
+  const randomCharacters = (data) => {
+    for (let i = 0; i < 30; i++) {
+      setAllCharactersNames((state) => [...state, data[randomNum()].name]);
+    }
+  };
 
   const randomNum = () => {
     return Math.floor(Math.random() * 50) + 0;
   };
 
-  const callAPI = async () => {
+  const getAllCharacter = async () => {
+    setAllCharactersNames([]);
+    try {
+      const res = await fetch(`https://api.disneyapi.dev/characters`);
+      const data = await res.json();
+      randomCharacters(data.data);
+    } catch (err) {
+      console.log("--------", err);
+    }
+  };
+
+  const getRandomCharacter = async () => {
     setCharacter([]);
     setAnswer("");
     try {
       const res = await fetch(`https://api.disneyapi.dev/characters/${randomNum()}`);
       const data = await res.json();
-      console.log("🚀 ~ file: Stage.js ~ line 23 ~ callAPI ~ data", data);
       setCharacter(data);
       setAnswer(data.name);
-      //randomCharacters(data.data);
     } catch (err) {
-      //Rerun the api call
       console.log("--------", err);
     }
   };
+
+  useEffect(() => {
+    getAllCharacter();
+  }, []);
 
   return (
     <main>
@@ -47,11 +61,14 @@ const Stage = () => {
         <li>You will only have one guess per round</li>
         <li>At the end of the game you will be given a total score</li>
       </ul>
-      <button onClick={() => callAPI()}>Start</button>
+      <button onClick={() => getRandomCharacter()}>Start</button>
       <div>
         <p>Play area</p>
         <div>{character.imageUrl && <Image width="450" height="450" src={character.imageUrl} alt="guess who" />}</div>
-        <div>Name - {answer}</div>
+        <div>Name 1 - {answer}</div>
+        <div>Name 2 - {allCharactersNames[randomNum()]}</div>
+        <div>Name 3 - {allCharactersNames[randomNum()]}</div>
+        <div>Name 4 - {allCharactersNames[randomNum()]}</div>
       </div>
     </main>
   );
