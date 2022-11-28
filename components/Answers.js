8626883a68dcd2d56105cb500dddde90a6f8d14b
Mohammed Ansir <1854992+MoAnsir@users.noreply.github.com>
2022-11-28 15:19:33 +0000

    refactor comp

---
+++ components/Answers.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Answers = ({ allAnswers, checkAnswer }) => {
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+
+  return (
+    <>
+      {shuffle(allAnswers).map((name, index) => (
+        <button htmlFor="my-modal" className="btn" key={index} onClick={() => checkAnswer(name)}>
+          {name}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default Answers;
